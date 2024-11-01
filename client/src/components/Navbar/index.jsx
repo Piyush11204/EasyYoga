5f9f78fe2a64d42dbd410b227ced2ca30ac0b48b
Piyush Yadav <piyushkrishna11@gmail.com>
2024-11-01 23:12:56 +0530

    Update index.jsx

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import {  Link } from 'react-router-dom';
 import axios from "axios";
-import { Menu, LogOut, User } from 'lucide-react'; // Import Lucide icons
+import { Menu, LogOut, User } from 'lucide-react'; // Import Lucide
+ icons
 import YogaLogo from "../../img/YogaLogin.png";
 
 

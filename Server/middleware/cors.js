c1d9782f068233f8d6ccfea860f2285db1e77253
Piyush11204 <138059020+Piyush11204@users.noreply.github.com>
2025-02-23 00:07:42 +0530

    Update CORS middleware to allow all origins and restrict methods to GET and POST

--- Server/middleware/cors.js
+++ Server/middleware/cors.js
@@ -1,9 +1,9 @@
 const cors = require('cors');
 
 const corsMiddleware = cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 });
 
 module.exports = corsMiddleware;
